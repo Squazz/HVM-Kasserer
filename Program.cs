edd54cbb7a59dee3a486e22381ae790dc50aec87
Squazz <GitHub@squazz.dk>
2025-05-11 23:07:39 +0200

    Now checking if CPR & Address has a match, and if not, ask to have it added to the list

--- Program.cs
+++ Program.cs
@@ -1,18 +1,17 @@
 ﻿using System.Globalization;
 using System.Text.RegularExpressions;
 using ClosedXML.Excel;
-using DocumentFormat.OpenXml.Spreadsheet;
-using DocumentFormat.OpenXml.Vml.Office;
 
 class Program
 {
     private static string mobilePayFilepath = @"D:\Dropbox\HVM - Kasserer\Indsamlinger\2025 Indsamlinger\transactions-report-1_1_2024-12_29_2024.csv";
+    private static string bankPosteringerFilepath = @"C:\Users\kaspe\Downloads\eksport.csv";
 
-    static string exclusionsFilePath = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "mobilePayExclusions.txt");
-    static string matchesFilePath = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "cprToAddressMatches.xlsx");
+    static string exclusionsFilePath = @"D:\Dropbox\HVM - Kasserer\Program-kode\HVM Kasserer\mobilePayExclusions.txt";
+    static string matchesFilePath = @"D:\Dropbox\HVM - Kasserer\Program-kode\HVM Kasserer\cprToAddressMatches.xlsx";
 
-    static List<string> exclusions = LoadExclusionsFromFile();
-    static List<CprToSenderMatch> matches = LoadMatchesFromFile();
+    static List<string> mobilePayExclusions = LoadExclusionsFromFile();
+    static List<CprToSenderMatch> cprToSenderMatches = LoadMatchesFromFile();
 
     private static List<CprToSenderMatch> LoadMatchesFromFile()
     {
@@ -73,8 +72,131 @@ class Program
 
     static void Main()
     {
-        var test = matches;
+        var test = cprToSenderMatches;
+        HandleBankPosteringer();
         SummarizeMobilePayTransactions();
+    }
+
+    private static void HandleBankPosteringer()
+    {
+        // Get all of the transactions parsed to an object we can use
+        var transactionData = new List<BankPostering>();
+        using (var reader = new StreamReader(bankPosteringerFilepath))
+        {
+            // Skip the header row
+            reader.ReadLine();
+
+            CultureInfo cultureInfo = CultureInfo.GetCultureInfo("da-DK");
+
+            while (!reader.EndOfStream)
+            {
+                var line = reader.ReadLine();
+                if (line == null) break;
+
+                string[] values = line.Split(';');
+
+                transactionData.Add(new BankPostering
+                {
+                    Date = DateTime.Parse(values[0], cultureInfo),
+                    Message = values[1],
+                    Amount = decimal.Parse(values[2], cultureInfo),
+                    Address = values[6]
+                });
+            }
+        }
+
+        // Only use transactions with positive amounts
+        transactionData = transactionData.Where(t => t.Amount > 0).ToList();
+
+        // traverse transactionData and find all destinct addresses, that is a multiline address
+        var addresses = transactionData
+            .Select(t => t.Address)
+            .Distinct()
+            .Where(a => a.Contains(", "))
+            .ToList();
+
+        // For each address, find the corresponding CPR number, if none are found ask the user for the CPR number and add the pair to the file
+        foreach (var address in addresses)
+        {
+            var cprMatch = cprToSenderMatches.FirstOrDefault(m => m.SenderAddress.Equals(address, StringComparison.OrdinalIgnoreCase));
+            if (cprMatch == null)
+            {
+                Console.WriteLine($"No CPR match found for address: {address}");
+                Console.Write("Do you want to add a match entry for that address? y/n: ");
+                string? addEntry = Console.ReadLine();
+                if (string.IsNullOrEmpty(addEntry) || addEntry?.ToLower() != "y")
+                {
+                    continue; // Skip to the next address
+                }
+
+                // if two deposits are found on the same day, with the same address but different messages, it indicates we should add two entries as two people can live on the same address
+                var depositsGroupedByDate = transactionData
+                    .Where(t => t.Address.Equals(address, StringComparison.OrdinalIgnoreCase))
+                    .GroupBy(t => t.Date.Date)
+                    .ToList();
+
+                // If Any of the entries in the variable deposists has multiple entries, we should ask for each entry and present both address and message
+                var depositsWithMutlipleEntriesOnSameDay = depositsGroupedByDate.Where(x => x.Count() > 1).ToList();
+
+                if (depositsWithMutlipleEntriesOnSameDay.Any(x => x.Count() > 1))
+                {
+                    var firstDeposit = depositsGroupedByDate.First();
+                    foreach (var deposit in firstDeposit)
+                    {
+                        string? cpr = null;
+                        while (string.IsNullOrWhiteSpace(cpr))
+                        {
+                            Console.Write($"Please enter the CPR number for this message: \"{deposit.Message}\": ");
+                            cpr = Console.ReadLine();
+
+                            if (string.IsNullOrWhiteSpace(cpr))
+                            {
+                                Console.WriteLine("Input cannot be empty. Please try again.");
+                            }
+                        }
+
+                        using var workbook = new XLWorkbook(matchesFilePath);
+                        var worksheet = workbook.Worksheet(1);
+
+                        var rowNumber = worksheet.RowsUsed().Last().RowNumber();
+                        var newRow = worksheet.Row(rowNumber + 1);
+
+                        newRow.Cell(1).Value = cpr;
+                        newRow.Cell(2).Value = address;
+                        newRow.Cell(3).Value = deposit.Message;
+
+                        // Save the workbook
+                        workbook.Save();
+                    }
+                }
+                else
+                {
+                    string? cpr = null;
+                    while (string.IsNullOrWhiteSpace(cpr))
+                    {
+                        Console.Write($"Please enter the CPR number: ");
+                        cpr = Console.ReadLine();
+
+                        if (string.IsNullOrWhiteSpace(cpr))
+                        {
+                            Console.WriteLine("Input cannot be empty. Please try again.");
+                        }
+                    }
+
+                    using var workbook = new XLWorkbook(matchesFilePath);
+                    var worksheet = workbook.Worksheet(1);
+
+                    var rowNumber = worksheet.RowsUsed().Last().RowNumber();
+                    var newRow = worksheet.Row(rowNumber + 1);
+
+                    newRow.Cell(1).Value = cpr;
+                    newRow.Cell(2).Value = address;
+
+                    // Save the workbook
+                    workbook.Save();
+                }
+            }
+        }
     }
 
     static void SummarizeMobilePayTransactions()
@@ -84,7 +206,7 @@ class Program
         // Step 1: Identify TransactionIDs with exclusion keywords
         var excludedTransactionIDs = transactions
             .Where(t => t.Message != null &&
-                        exclusions.Any(keyword => NormalizeString(t.Message).Contains(NormalizeString(keyword))))
+                        mobilePayExclusions.Any(keyword => NormalizeString(t.Message).Contains(NormalizeString(keyword))))
             .Select(t => t.TransactionID)
             .Distinct()
             .ToHashSet();
@@ -449,6 +571,14 @@ class CprToSenderMatch
     public required string CPR { get; set; }
     public required string SenderAddress { get; set; }
     public List<string>? Keywords { get; set; }
+}
+
+class BankPostering
+{
+    public DateTime Date { get; set; }
+    public required string Message { get; set; }
+    public decimal Amount { get; set; }
+    public required string Address { get; set; }
 }
 
 class Transaction
