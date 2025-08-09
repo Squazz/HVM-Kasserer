5aec4aab1e2a0ee6fcf80b73cf2621605467f420
Squazz <GitHub@squazz.dk>
2025-08-09 22:27:19 +0200

    Cleanup

--- BankPosteringer.cs
+++ BankPosteringer.cs
@@ -1,10 +1,5 @@
 ﻿using ClosedXML.Excel;
-using System;
-using System.Collections.Generic;
 using System.Globalization;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace HVM_Kasserer
 {
